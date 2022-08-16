b413470a5e30e8fc496724144569aeee1c5809ed
gormm1980 <gormm1980@gmail.com>
2022-08-16 16:50:50 +0200

    added empire to the admin

--- Empire/admin.py
+++ Empire/admin.py
@@ -1,2 +1,27 @@
 from django.contrib import admin
 from Empire.models import Empire, EmpireCommanders, EmpireHeroes, EmpireBasicTroops, EmpireSpecialTroops, EmpireSingularTroops
+
+@admin.register(Empire)
+class EmpireAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(EmpireCommanders)
+class EmpireCommandersAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(EmpireHeroes)
+class EmpireHeroesAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(EmpireBasicTroops)
+class EmpireBasicTroopsAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(EmpireSpecialTroops) 
+class EmpireSpecialTroopsAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(EmpireSingularTroops)
+class EmpireSingularTroopsAdmin(admin.ModelAdmin):
+    pass 
+  
